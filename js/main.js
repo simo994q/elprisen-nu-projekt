d3a6380d65b539bb0c46f973be4ec0238bcd7d8c
simo994q <simonkn1@hotmail.com>
2023-11-01 12:29:54 +0100

    page changer wooooooooo

--- js/main.js
+++ js/main.js
@@ -240,3 +240,85 @@ getHistoryData('1')
 fetch(`https://www.elprisenligenu.dk/api/v1/prices/${currentYear}/${currentMonth}-${currentDate}_${priceClass}.json`)
     .then(response => response.json())
     .then(apiData => loadData(apiData))
+
+
+
+
+
+
+// Mobile view controller
+
+window.addEventListener('resize', () => {
+    handleResize()
+})
+
+const pageOne = document.getElementById('currentContainer')
+const pageTwo = document.getElementById('overviewContainer')
+const pageThree = document.getElementById('historyContainer')
+
+const setDisplay = (page) => {
+    switch (page) {
+        case 1:
+            pageOne.style.display = 'flex'
+            pageTwo.style.display = 'none'
+            pageThree.style.display = 'none'
+            break;
+        case 2:
+            pageOne.style.display = 'none'
+            pageTwo.style.display = 'block'
+            pageThree.style.display = 'none'
+            break;
+        case 3:
+            pageOne.style.display = 'none'
+            pageTwo.style.display = 'none'
+            pageThree.style.display = 'block'
+            break;
+    }
+}
+const fullDisplay = () => {
+    pageOne.style.display = 'flex'
+    pageTwo.style.display = 'block'
+    pageThree.style.display = 'block'
+}
+
+const navOne = document.getElementById('navOne')
+const navTwo = document.getElementById('navTwo')
+const navThree = document.getElementById('navThree')
+
+navOne.addEventListener('click', () => {
+    changeDisplay(1)
+})
+navTwo.addEventListener('click', () => {
+    changeDisplay(2)
+})
+navThree.addEventListener('click', () => {
+    changeDisplay(3)
+})
+let pageSelector = 1
+
+const changeDisplay = (page) => {
+    switch (page) {
+        case 1:
+            pageSelector = 1
+            handleResize()
+            break;
+        case 2:
+            pageSelector = 2
+            handleResize()
+            break;
+        case 3:
+            pageSelector = 3
+            handleResize()
+            break;
+    }
+}
+
+const handleResize = () => {
+    if (window.innerWidth < 850) {
+        setDisplay(pageSelector)
+    } else {
+        fullDisplay()
+    }
+}
+
+handleResize()
